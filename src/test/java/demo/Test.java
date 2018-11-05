f7445c724854fb32d622d8fde6b7cfd883bab927
Jason Xie <x403368945@126.com>
2018-11-05 22:22:57 +0800

    线程池调整

--- src/test/java/demo/Test.java
+++ src/test/java/demo/Test.java
@@ -10,10 +10,5 @@ public class Test {
         System.out.println("Sheet1!A1:AB1".replaceAll("([A-Z]+)(\\d+):([A-Z]+)(\\d+)", "\\$$1\\$$2:\\$$3\\$$4"));
         System.out.println(new CellAddress("A1").formatAsString());
 
-        System.out.println("a.xlsx".matches("[\\s\\S]*\\.xls(x)?$"));
-        System.out.println("a.xls".matches("[\\s\\S]\\.xls(x)?$"));
-        System.out.println("a.xlss".matches("[\\s\\S]\\.xls(x)?$"));
-        System.out.println("rL8aqMy3ZV8yiB/hui2WUA==".replaceAll("\\w", "*"));
-
     }
 }
